4e3f17ed261d3e4b9ed6d364d49878c60ef7678e
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-05-17 19:48:55 -0500

    fetch data from mysql database for given search word

--- word.js
+++ word.js
@@ -1,12 +1,15 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "grootgroot"
+    host: "localhost",
+    user: "root",
+    password: "grootgroot"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+con.connect(function (err) {
+    if (err) throw err;
+    con.query("select * from entries.entries WHERE word='king';", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
 });
