787d5de03afac09bdfb3982a8c73115779049953
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-05-17 21:57:57 -0500

    final code of online dict project

--- word.js
+++ word.js
@@ -1,15 +1,17 @@
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
-var con = mysql.createConnection({
+var con = {
     host: "localhost",
     user: "root",
     password: "grootgroot"
-});
+};
 
-con.connect(function (err) {
-    if (err) throw err;
-    con.query("select * from entries.entries WHERE word='king';", function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-    });
-});
+// con.connect(function (err) {
+//     if (err) throw err;
+//     con.query("select * from entries.entries WHERE word='king';", function (err, result, fields) {
+//         if (err) throw err;
+//         console.log(result);
+//     });
+// });
+
+module.exports = con;
